4d658d6b0545c3e6a5dd91814851f466257f6122
Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>
2023-12-12 10:30:03 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,19 +1,35 @@
+const urlParams = new URLSearchParams(location.search);
+if (urlParams.has('theme'))
+	location.replace(location.origin + location.pathname);
+const referrerParams = new URLSearchParams(document.referrer.substring(document.referrer.indexOf('?')));
+if (referrerParams.has('theme'))
+	theme(referrerParams.get('theme'));
+
+function navigate(url) {
+	location = url + '?theme=' + (document.body.getAttribute('theme') || 'light');;
+}
 function switchTheme() {
 	if (document.body.getAttribute('theme') === 'light') {
-		document.body.setAttribute('theme', 'dark');
-		document.getElementById('theme-icon').setAttribute('src', 'icons/theme-dark.svg');
-		document.getElementById('theme-icon').setAttribute('alt', 'switch theme from dark to light');
-		return true;
+		theme('dark');
+		return;
 	}
 	if (document.body.getAttribute('theme') === 'dark') {
+		theme('light');
+		return;
+	}
+	theme('light');
+}
+function theme(value) {
+	if (value === 'light') {
 		document.body.setAttribute('theme', 'light');
 		document.getElementById('theme-icon').setAttribute('src', 'icons/theme-light.svg');
 		document.getElementById('theme-icon').setAttribute('alt', 'switch theme from light to dark');
-		return true;
+		return;
 	}
-	console.log('theme not found');
-	document.body.setAttribute('theme', 'light');
-	document.getElementById('theme-icon').setAttribute('src', 'icons/theme-light.svg');
-	document.getElementById('theme-icon').setAttribute('alt', 'switch theme from light to dark');
-	return false;
+	if (value === 'dark') {
+		document.body.setAttribute('theme', 'dark');
+		document.getElementById('theme-icon').setAttribute('src', 'icons/theme-dark.svg');
+		document.getElementById('theme-icon').setAttribute('alt', 'switch theme from dark to light');
+		return;
+	}
 }
