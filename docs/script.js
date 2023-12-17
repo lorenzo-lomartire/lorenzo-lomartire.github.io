063384fe4dd0c999a5263088193f14ac0eeab585
Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>
2023-12-17 20:57:24 +0100

    Update script.js
    Signed-off-by: Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>

--- docs/script.js
+++ docs/script.js
@@ -78,3 +78,9 @@ function onSignIn(googleUser) {
 	console.log('Image URL: ' + profile.getImageUrl());
 	console.log('Email: ' + profile.getEmail());
 }
+function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function () {
+		console.log('User signed out.');
+	});
+}
