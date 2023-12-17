7f2f2c82ec727fefe00661783bd605e4791f7289
Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>
2023-12-17 20:56:20 +0100

    Update script.js
    Signed-off-by: Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>

--- docs/script.js
+++ docs/script.js
@@ -66,8 +66,15 @@ function text(lang) {
 	fetch('./text/' + lang + '.json')
 	.then(response => response.json())
 	.then(data => {
-	for (let id in data) {
-		document.getElementById(id).textContent = data[id];
-	}
-});
+		for (let id in data) {
+			document.getElementById(id).textContent = data[id];
+		}
+	});
+}
+function onSignIn(googleUser) {
+	var profile = googleUser.getBasicProfile();
+	console.log('ID: ' + profile.getId());
+	console.log('Name: ' + profile.getName());
+	console.log('Image URL: ' + profile.getImageUrl());
+	console.log('Email: ' + profile.getEmail());
 }
