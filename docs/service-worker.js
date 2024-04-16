6551efbeff4cb8a70bdccbe713126d5708a9e766
Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>
2024-04-16 21:41:05 +0200

    Update service-worker.js
    Signed-off-by: Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -4,8 +4,11 @@ self.addEventListener('install', function(event) {
 			console.log('Opened cache');
 			return cache.addAll([
 				'/',
+				'https://www.googletagmanager.com/gtm.js?id=GTM-TDK9KB9N',
+				'https://www.googletagmanager.com/gtag/js?id=G-GMTBSPBHXL',
 				'/script.js',
 				'/style.css',
+				'/icons/main/icon.webp',
 				'/icons/home.svg',
 				'/icons/music.svg',
 				'/icons/lang/en.svg',
