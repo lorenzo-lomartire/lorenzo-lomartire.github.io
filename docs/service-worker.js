f3c3953449a3ca5dc035c4ca3a72409773786dd9
Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>
2024-04-16 21:27:21 +0200

    Update service-worker.js
    Signed-off-by: Lorenzo Lomartire <97639913+LL-10@users.noreply.github.com>

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,9 +1,22 @@
 self.addEventListener('install', function(event) {
 	event.waitUntil(
 		caches.open('lorenzo-lomartire.github.io').then(function(cache) {
+			console.log('Opened cache');
 			return cache.addAll([
-				'/'
+				'/',
+				'/script.js',
+				'/style.css',
+				'/icons',
+				'/text'
 			]);
+		})
+	);
+});
+
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches.match(event.request).then(function(response) {
+			return response ? response : fetch(event.request);
 		})
 	);
 });
